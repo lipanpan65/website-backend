007629f1192ff70aec0ed83abe3a4265e4931c14
lipanpan03 <lipanpan03@58.com>
2021-08-01 00:44:53 +0800

    create projects

---
+++ website/website/settings/dev.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/31 11:09 下午
+# @Author  : lipanpan03
+# @Email  : lipanpan03@58.com
+# @File  : dev.py
+
+
+from .common import *
+
+DEV = True
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'website',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '39.105.100.168',
+        'PORT': '3306',
+    }
+}
